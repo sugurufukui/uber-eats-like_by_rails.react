7e5c4281fa5c40096cfe9c97d175255a417dd9a3
sugurufukui <2ar.1225@gmail.com>
2022-05-29 13:58:46 +0900

    model記述

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :foods
+  #特定の店舗がどんな仮注文を持っているか、Restaurantモデルから直接Orderモデルを参照することがないのでコメントアウト
+  # has_many :line_foods, through: :foods
+
+  validates :name, :fee, :time_required, presence :true
+  validates :name, length: { maximum: 30 }
+  validates :fee, numericality: { greater_than: 0 }
+end
